d6a8c9ee31ea9412ab5081c4ecccb1cd9702b38b
rskopyl <rskopyl@gmail.com>
2023-04-19 15:50:01 +0300

    Resources & Entry Point
    - added MainActivity
    - added color resources
    - added font resources
    - added day & night themes
    - created mipmap

---
+++ app/src/main/java/com/rskopyl/notes/ui/MainActivity.java
@@ -0,0 +1,20 @@
+package com.rskopyl.notes.ui;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.rskopyl.notes.databinding.ActivityMainBinding;
+
+public final class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+    }
+}
